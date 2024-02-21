61600d502c1bf9990019f35e7db80928d1cbd9c3
dzaima <dzaimagit@gmail.com>
2024-02-21 16:37:46 +0200

    Vec.ofExCollection

--- src/dzaima/utils/Vec.java
+++ src/dzaima/utils/Vec.java
@@ -202,6 +202,9 @@ public final class Vec<T> implements Iterable<T> {
   public static <T> Vec<T> ofCollection(Collection<T> vs) {
     return ofReuse((T[]) vs.toArray(new Object[0]));
   }
+  public static <R, T extends R> Vec<R> ofExCollection(Collection<T> vs) {
+    return ofReuse((R[]) vs.toArray(new Object[0]));
+  }
   
   public void clear() {
     arr = (T[]) EMPTY;
